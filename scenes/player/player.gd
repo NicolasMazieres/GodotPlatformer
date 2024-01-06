40a0740164daf62a076acd6d0f8306259ce239ef
Nicolas Mazieres <111435820+NicolasMazieres@users.noreply.github.com>
2024-01-06 22:51:56 +0100

    feat: new animations and jump mechanic

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,22 +1,32 @@
 extends CharacterBody2D
 
-const SPEED = 150.0
-const JUMP_VELOCITY = -350.0
+const SPEED = 100.0
+const JUMP_VELOCITY = -175.0
 
 @onready var ap = $AnimationPlayer
 @onready var sprite = $Sprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var jump_max_duration : float = 0.25
+var jump_duration: float = 0.0
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		if Input.is_action_just_released("jump"):
+			jump_duration = jump_max_duration
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_duration = 0
+	
+	if Input.is_action_pressed("jump") and not is_on_floor():
+		jump_duration += delta
+		if jump_duration < jump_max_duration:
+			velocity.y += - gravity * delta * 1.05
 
 	# Get the input direction and handle the movement/deceleration.
 	var direction = Input.get_axis("left", "right")
@@ -28,12 +38,14 @@ func _physics_process(delta):
 	switch_direction(direction)
 	
 	move_and_slide()
-	update_animations(direction)
+	update_movement_animations(direction)
 	
-func update_animations(direction):
+func update_movement_animations(direction):
 	if is_on_floor():
 		if direction != 0:
-			ap.play("walk")
+			ap.play("run", -1, 2)
+		else:
+			ap.play("idle", -1, 0.5)
 	else:
 		if velocity.y < 0:
 			ap.play("jump")
