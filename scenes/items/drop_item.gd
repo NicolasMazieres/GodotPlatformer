478002132557c6d6d218c98163f46a9d901afac1
Nicolas Mazieres <111435820+NicolasMazieres@users.noreply.github.com>
2024-02-21 23:26:32 +0100

    feat: added item for enemy drop

---
+++ scenes/items/drop_item.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var rotation_speed: int = 40
+var initial_speed: int = randi_range(100,150)
+var initial_direction: Vector2 = Vector2(randi(), randi()).normalized()
+
+func _ready():
+	velocity = initial_direction * initial_speed
+
+func _process(delta):
+	rotation += rotation_speed * delta
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta /2
+		move_and_slide()
+
+func _on_area_2d_body_entered(_body):
+	queue_free()
